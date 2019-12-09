d780194be1cb0771fd849d8c6646bcdd524684e4
grisha <codpure@gmail.com>
2019-12-09 13:23:29 +0200

    stats

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -11,12 +11,17 @@ class Content extends React.Component {
 
 
     render() {
+
+        let stats = <div className='stats-area'>Add some values to graph</div>
+        if(this.props.data.length > 0)
+            stats = <Stats valueType={this.props.valueType} data={this.props.data}/>;
+        
         return (
             <>
             <div className='content'>
                 <Graph data={this.props.data} valueType={this.props.valueType}/>
                 {/* <Divider /> */}
-                <Stats valueType={this.props.valueType} data={this.props.data}/>
+                {stats}
             </div>
             <Add title={this.props.title} newValue={this.props.newValue} category={this.props.category} graph={this.props.graph}/>
             </>
--- src/components/Content/Stats/Stats.js
+++ src/components/Content/Stats/Stats.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../../../App.css';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 
 class Stats extends React.Component {
     constructor(props) {
@@ -9,10 +11,36 @@ class Stats extends React.Component {
     render() {
         return (
             <div className='stats-area'>
-                <p> hey </p>
+                <Typography variant="h6" className='header-title'>Stats:</Typography>
+                <Divider />
+                <p>Type: {this.props.valueType}</p>
+                <p>Last: {this.props.data[this.props.data.length -1].y}</p>
+                <p>Highest: {getHighest(this.props.data)}</p>
+                <p>Lowest: {getLowest(this.props.data)}</p>
+                <p>Average: {getAverage(this.props.data)}</p>
             </div>
         );
     }
 }
 
 export default Stats;
+
+
+function getHighest(data){
+    let a = Math.max.apply(Math, data.map(function(o) {
+        return o.y;
+    }))
+
+    return a;
+}
+
+function getLowest(data){
+    let a = Math.min.apply(Math, data.map(function(o) {
+        return o.y;
+    }))
+    return a;
+}
+
+function getAverage(data){
+    return data.reduce((r, c) => r + c.y, 0) / data.length;
+}
